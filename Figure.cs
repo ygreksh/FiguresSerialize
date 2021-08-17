35521c0737b64a8d6c19aee4ef82716d322fffd5
ygrek <ygreksh@gmail.com>
2021-08-17 11:43:23 +0300

    json ready

---
+++ Figure.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace FiguresSerialize
+{
+    [Serializable]
+    public class Figure
+    {
+        public string Name { get; set; }
+        public int SideCount { get; set; }
+        public double SideLenght { get; set; }
+        public Figure Figure1 { get;  set; }
+
+        public static string SerializeToJSON(Figure figure)
+        {
+            string json = JsonConvert.SerializeObject(figure);
+            return json;
+        }
+
+        public static Figure DeserializeFromJSON(string json)
+        {
+            Figure figure = JsonConvert.DeserializeObject<Figure>(json);
+            return figure;
+        }
+        
+        public static string SerializeToXML(Figure figure)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(Figure));
+            string xml = null;
+            return xml;
+        }
+
+        public void DeserializeFromXML(string xml)
+        {
+            
+        }
+        
+        public byte[] SerializeToBinary()
+        {
+            byte[] binary = null;
+            return binary;
+        }
+
+        public void DeserializeFronBinary(byte[] binary)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace FiguresSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
