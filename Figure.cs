4541bdd079413e004c836e709cb17eb34a11dafa
ygrek <ygreksh@gmail.com>
2021-08-17 15:38:48 +0300

    Homework Figures done

--- Figure.cs
+++ Figure.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
@@ -14,46 +15,46 @@ namespace FiguresSerialize
         public double SideLenght { get; set; }
         public Figure Figure1 { get;  set; }
 
-        public static void SerializeToJSON(FileStream fs, Figure figure)
+        public static void SerializeToJSON(FileStream fs, List<Figure> listOfFigures)
         {
-            string json = JsonConvert.SerializeObject(figure);
+            string json = JsonConvert.SerializeObject(listOfFigures);
             byte[] arrayjson = System.Text.Encoding.Default.GetBytes(json);
             fs.Write(arrayjson,0,arrayjson.Length);
         }
 
-        public static Figure DeserializeFromJSON(FileStream fs)
+        public static List<Figure> DeserializeFromJSON(FileStream fs)
         {
             byte[] arrayjson = new byte[fs.Length];
             fs.Read(arrayjson,0,arrayjson.Length);
             string json = System.Text.Encoding.Default.GetString(arrayjson);
-            Figure figure = JsonConvert.DeserializeObject<Figure>(json);
-            return figure;
+            List<Figure> listOfFigures = JsonConvert.DeserializeObject<List<Figure>>(json);
+            return listOfFigures;
         }
         
-        public static void SerializeToXML(FileStream fs, Figure figure)
+        public static void SerializeToXML(FileStream fs, List<Figure> listOfFigures)
         {
-            XmlSerializer formatter = new XmlSerializer(typeof(Figure));
-            formatter.Serialize(fs, figure);
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Figure>));
+            formatter.Serialize(fs, listOfFigures);
         }
 
-        public Figure DeserializeFromXML(FileStream fs)
+        public static List<Figure> DeserializeFromXML(FileStream fs)
         {
-            XmlSerializer formatter = new XmlSerializer(typeof(Figure));
-            Figure figure = (Figure)formatter.Deserialize(fs);
-            return figure;
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Figure>));
+            List<Figure> listOfFigures = (List<Figure>)formatter.Deserialize(fs);
+            return listOfFigures;
         }
         
-        public static void SerializeToBinary(FileStream fs, Figure figure)
+        public static void SerializeToBinary(FileStream fs, List<Figure> listOfFigures)
         {
             BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(fs, figure);
+            formatter.Serialize(fs, listOfFigures);
         }
 
-        public static Figure DeserializeFronBinary(FileStream fs)
+        public static List<Figure> DeserializeFronBinary(FileStream fs)
         {
             BinaryFormatter formatter = new BinaryFormatter();
-            Figure figure = (Figure)formatter.Deserialize(fs);
-            return figure;
+            List<Figure> listOfFigures = (List<Figure>)formatter.Deserialize(fs);
+            return listOfFigures;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace FiguresSerialize
@@ -7,7 +9,76 @@ namespace FiguresSerialize
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            List<Figure> listOfFigures = new List<Figure>();
+            Figure hexagon = new Figure() { Name = "Hexagon", SideCount = 6, SideLenght = 1 };
+            Figure octagon = new Figure() { Name = "Oktagon", SideCount = 8, SideLenght = 1 };
+            hexagon.Figure1 = octagon;
+            //octagon.Figure1 = octagon;
+            
+            listOfFigures.Add(new Figure() { Name = "Triangle", SideCount = 3, SideLenght = 1 });
+            listOfFigures.Add(new Figure() { Name = "Square", SideCount = 4, SideLenght = 1 });
+            listOfFigures.Add(new Figure() { Name = "Pentagon", SideCount = 5, SideLenght = 1 });
+            listOfFigures.Add(hexagon);
+            listOfFigures.Add(octagon);
+            
+
+            string path = Path.Combine("Fugures");
+            DirectoryInfo MainDirectoryInfo = new DirectoryInfo(path);
+            string FiguresfileNameJSON = "FiguresJSON.txt";
+            string FiguresfileNameXML = "FiguresXML.xml";
+            string FiguresfileNameBinary = "FiguresBinary.bin";
+            if (!MainDirectoryInfo.Exists)
+            {
+                MainDirectoryInfo.Create();
+            }
+            //  JSON
+            using (FileStream fileStream = new FileStream($"{path}\\{FiguresfileNameJSON}", FileMode.Create))
+            {
+                Figure.SerializeToJSON(fileStream, listOfFigures);
+            }
+
+            List<Figure> listFromJSON;
+            using (FileStream fileStream = new FileStream($"{path}\\{FiguresfileNameJSON}", FileMode.Open))
+            {
+                listFromJSON = Figure.DeserializeFromJSON(fileStream);
+            }
+            Console.WriteLine("From JSON:");
+            foreach (var figure in listFromJSON)
+            {
+                Console.WriteLine($"    {figure.Name} ({figure.SideCount},{figure.SideLenght})");
+            }
+            //  XML
+            using (FileStream fileStream = new FileStream($"{path}\\{FiguresfileNameXML}", FileMode.Create))
+            {
+                Figure.SerializeToXML(fileStream, listOfFigures);
+            }
+            Console.WriteLine("From XML:");
+            List<Figure> listFromXML;
+            using (FileStream fileStream = new FileStream($"{path}\\{FiguresfileNameXML}", FileMode.Open))
+            {
+                listFromXML = Figure.DeserializeFromXML(fileStream);
+            }
+            foreach (var figure in listFromXML)
+            {
+                Console.WriteLine($"    {figure.Name} ({figure.SideCount},{figure.SideLenght})");
+            }
+            //  Binary
+            using (FileStream fileStream = new FileStream($"{path}\\{FiguresfileNameBinary}", FileMode.Create))
+            {
+                Figure.SerializeToBinary(fileStream, listOfFigures);
+            }
+            Console.WriteLine("From BIN:");
+            List<Figure> listFromBinary;
+            using (FileStream fileStream = new FileStream($"{path}\\{FiguresfileNameBinary}", FileMode.Open))
+            {
+                listFromBinary = Figure.DeserializeFronBinary(fileStream);
+            }
+            foreach (var figure in listFromBinary)
+            {
+                Console.WriteLine($"    {figure.Name} ({figure.SideCount},{figure.SideLenght})");
+            }
+            
+         }
     }
+
 }
