943d94cca7e402a6773572207aa7d9b8a981490a
ygrek <ygreksh@gmail.com>
2021-08-17 12:16:21 +0300

    Figure.cs is ready

--- Figure.cs
+++ Figure.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 
@@ -12,39 +14,46 @@ namespace FiguresSerialize
         public double SideLenght { get; set; }
         public Figure Figure1 { get;  set; }
 
-        public static string SerializeToJSON(Figure figure)
+        public static void SerializeToJSON(FileStream fs, Figure figure)
         {
             string json = JsonConvert.SerializeObject(figure);
-            return json;
+            byte[] arrayjson = System.Text.Encoding.Default.GetBytes(json);
+            fs.Write(arrayjson,0,arrayjson.Length);
         }
 
-        public static Figure DeserializeFromJSON(string json)
+        public static Figure DeserializeFromJSON(FileStream fs)
         {
+            byte[] arrayjson = new byte[fs.Length];
+            fs.Read(arrayjson,0,arrayjson.Length);
+            string json = System.Text.Encoding.Default.GetString(arrayjson);
             Figure figure = JsonConvert.DeserializeObject<Figure>(json);
             return figure;
         }
         
-        public static string SerializeToXML(Figure figure)
+        public static void SerializeToXML(FileStream fs, Figure figure)
         {
             XmlSerializer formatter = new XmlSerializer(typeof(Figure));
-            string xml = null;
-            return xml;
+            formatter.Serialize(fs, figure);
         }
 
-        public void DeserializeFromXML(string xml)
+        public Figure DeserializeFromXML(FileStream fs)
         {
-            
+            XmlSerializer formatter = new XmlSerializer(typeof(Figure));
+            Figure figure = (Figure)formatter.Deserialize(fs);
+            return figure;
         }
         
-        public byte[] SerializeToBinary()
+        public static void SerializeToBinary(FileStream fs, Figure figure)
         {
-            byte[] binary = null;
-            return binary;
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, figure);
         }
 
-        public void DeserializeFronBinary(byte[] binary)
+        public static Figure DeserializeFronBinary(FileStream fs)
         {
-            
+            BinaryFormatter formatter = new BinaryFormatter();
+            Figure figure = (Figure)formatter.Deserialize(fs);
+            return figure;
         }
     }
 }
